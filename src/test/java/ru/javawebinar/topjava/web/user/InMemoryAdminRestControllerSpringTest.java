cffaaa15424d19ff9ef8d77192d46024d9aed24e
yrkash <yrkash@gmail.com>
2023-02-19 22:11:56 +0300

    hw03_init

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -22,7 +22,7 @@ public class InMemoryAdminRestControllerSpringTest {
     @Autowired
     private AdminRestController controller;
 
-//    @Autowired
+    @Autowired
     private InMemoryUserRepository repository;
 
     @Before
