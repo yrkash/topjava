e939ab85e7c7ede7661229c6d75e7d6530f13c37
yrkash <yrkash@gmail.com>
2023-03-08 21:37:55 +0300

    hw05_dataJpa

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,11 +1,14 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
@@ -18,26 +21,38 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        Meal result = meal;
+        User user = new User();
+        user.setId(userId);
+        result.setUser(user);
+//        meal.setUser(em.getReference(User.class, userId));
+        if (meal.isNew()) {
+            return crudRepository.save(result);
+        }
+        return get(meal.id(), userId) == null ? null : crudRepository.save(meal);
+
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id,userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
+        Optional<Meal> meal = crudRepository.findById(id);
+        if (meal.isPresent() && meal.get().getUser().getId() == userId) return meal.get();
         return null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+
+        return crudRepository.getMealsByAllSortedContainingAndUser(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.getMealsByAllSortedContainingAndDateTime(startDateTime, endDateTime, userId);
     }
 }
