ad96a74080259b321598e7db5a703b0a7b2a9f82
yrkash <yrkash@gmail.com>
2023-03-22 22:06:55 +0300

    HW06_1_3 (JspMealController)

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -4,9 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.web.SecurityUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/meals")
@@ -29,7 +37,30 @@ public class JspMealController {
         return "meals";
     }
 
-    @DeleteMapping("/{id}")
+    @PostMapping("/update/{id}")
+    public String update(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        int id = Integer.parseInt(paramId);
+        int userId = SecurityUtil.authUserId();
+        service.update(service.get(id,userId),userId);
+        return "redirect:/meals";
+    }
+
+    @PostMapping()
+    public String create(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+        int userId = SecurityUtil.authUserId();
+
+        service.create(meal,userId);
+        return "redirect:/meals";
+
+    }
+
+    @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable("id") int id) {
         int userId = SecurityUtil.authUserId();
         service.delete(id, userId);
