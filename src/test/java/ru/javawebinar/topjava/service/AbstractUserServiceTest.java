1ca98134b9b0c4877caff089f4027df8dcff5586
yrkash <yrkash@gmail.com>
2023-03-23 19:58:44 +0300

    7_08_HW06_disable_tests_cache

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,14 +1,10 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.dao.DataAccessException;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
@@ -27,17 +23,18 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     @Autowired
     private CacheManager cacheManager;
 
-    @Autowired
-    @Lazy
-    protected JpaUtil jpaUtil;
 
     @Before
     public void setup() {
 
         cacheManager.getCache("users").clear();
-        if (isJpaBased()) {
+        /*if (isJpaBased()) {
             jpaUtil.clear2ndLevelHibernateCache();
-        }
+        }*/
+    }
+
+    private boolean isJpaBased() {
+        return false;
     }
 
     @Test
