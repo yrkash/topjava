3e53bd654bc9489e3b6797f4bc9f8014ce25c78d
yrkash <yrkash@gmail.com>
2023-03-24 23:14:48 +0300

    7_09_controller_test

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,10 +1,14 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.dao.DataAccessException;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+//import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
@@ -20,17 +24,20 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     @Autowired
     protected UserService service;
 
-    @Autowired
+    @Autowired(required = false)
     private CacheManager cacheManager;
 
+   /* @Autowired
+    @Lazy
+    protected JpaUtil jpaUtil;*/
 
     @Before
     public void setup() {
 
         cacheManager.getCache("users").clear();
-        /*if (isJpaBased()) {
-            jpaUtil.clear2ndLevelHibernateCache();
-        }*/
+        if (isJpaBased()) {
+//            jpaUtil.clear2ndLevelHibernateCache();
+        }
     }
 
     private boolean isJpaBased() {
